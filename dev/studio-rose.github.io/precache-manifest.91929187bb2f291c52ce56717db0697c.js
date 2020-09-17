bb89cf8d08032b2aef5a1b62c99cead98b74c01e
Jake Gathof <jakegathof@jakes-air.attlocal.net>
2020-09-16 23:23:47 -0500

    Updates

---
+++ dev/studio-rose.github.io/precache-manifest.91929187bb2f291c52ce56717db0697c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc6f390557ce355e55a5864d7ece55ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "698beea02dbf56243d5a",
+    "url": "/static/css/main.16c9f9be.chunk.css"
+  },
+  {
+    "revision": "39ba3b723fd9d62bd377",
+    "url": "/static/js/2.e4c9c102.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.e4c9c102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "698beea02dbf56243d5a",
+    "url": "/static/js/main.c15f574e.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "fa0a94fc9bf49ba78b12f8bdcb1459b1",
+    "url": "/static/media/logo.fa0a94fc.svg"
+  },
+  {
+    "revision": "a6a8154d3f2b85655f9fedfaa4826622",
+    "url": "/static/media/right.a6a8154d.svg"
+  }
+]);
