51c5b58fcd3bf0d624c6aeedcc9eb045d849f416
JacobGathof <jacobgathof@gmail.com>
2024-05-27 06:56:42 +0000

    Deploying to gh-pages from @ studio-rose/studio-rose.github.io@5e1596e94b5450e10a3f4c34bb340ccc63839a41 🚀

---
+++ _app/immutable/chunks/pandora.DTcTRQN7.js
@@ -0,0 +1,25 @@
+const n=`## Overview
+Pandora
+
+## See Also
+[Melody](../characters/melody)
+
+[Anubis](../characters/anubis)
+
+## History
+#### Before the Fall
+#### Fall of the Arcana
+
+#### Cycle 1
+Not present
+#### Cycle 2
+Not present
+#### Cycle 3
+Not present
+#### Cycle 4
+Died
+
+## Abilities
+
+## Notes and Trivia
+`;export{n as default};
---
+++ _app/immutable/chunks/paths.D0B8a-Ps.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1le0zjr)==null?void 0:s.base)??"/studio-rose.github.io";var e;const a=((e=globalThis.__sveltekit_1le0zjr)==null?void 0:e.assets)??t;export{a,t as b};
