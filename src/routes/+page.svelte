0f0fac83834a99d98e7c1c9e6b51cb5f694999bd
Gandalf <Gandalf@DESKTOP-V4DFHOO>
2024-06-08 01:50:42 -0700

    Testing hexagons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,19 @@
 <script>
-import {base} from "$app/paths";
+    import {base} from "$app/paths";
+
+
+    function generate_hexagon(height, center){
+        let ret = "";
+        let pi_over_3 = Math.PI / 3;
+        for(let i = 0; i < 6; i++){
+            let angle = i * pi_over_3;
+            let x_coordinate = center[0] + Math.sin(angle) * height;
+            let y_coordinate = center[1] + Math.cos(angle) * height;
+            ret += x_coordinate.toFixed(2) + " " + y_coordinate.toFixed(2) + " ";
+        }
+        return ret;
+    }
+
 </script>
 
 <h2>Arcana</h2>
@@ -15,7 +29,43 @@ import {base} from "$app/paths";
     <a href="{base}/characters/hammurabi">Hammurabi</a><br/>
 </div>
 
+<svg width="87" height="100" viewbox="0 0 87 100">
+    <polygon fill="currentColor" points={generate_hexagon(50, [44, 50])}></polygon>
+    <polygon class="test-path" fill="#000" points={generate_hexagon(45, [44, 50])}></polygon>
+    <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">Attributes</text>
+</svg>
+
+
 <hr/>
+
+<style>
+    svg {
+        height:100px;
+        width:100px;
+        filter: drop-shadow(0px 0px 10px rgba(255, 255, 255, 0.5));
+    }
+    svg polygon {
+        transition: 1.0s;
+        color: #aaaaaaff;
+        transform-origin: 50% 50%;
+    }
+    svg:hover polygon {
+        transform: rotate(180deg);
+        color: #0004;
+    }
+
+    .test-path{
+        fill:#fcd;
+        scale: 0;
+    }
+
+    svg:hover .test-path {
+        fill:#fcd;
+        scale: 100%;
+    }
+
+
+</style>
 
 <!--
 <h2>Relics</h2>
